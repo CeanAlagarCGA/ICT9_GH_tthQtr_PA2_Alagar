d85c99c47051098532fa63e72efe133af5a13dc7
CeanAlagarCGA <alagar.cf@obmontessori.edu.ph>
2026-03-17 12:34:42 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//participatioin activity 
+function compute_rank() {
+    
+    let math_score = Number(document.getElementById("math_score").value);
+
+    if (math_score >=95 && math_score <=100) {
+        window.alert("Gold");
+    } else if (math_score >=90 && math_score <=94) {
+        window.alert("Silver");
+    } else if (math_score >=85 && math_score <=89) {
+        window.alert("Bronze");
+    } else if (math_score >=80 && math_score <=84) {
+        window.alert("merit");
+    } else {
+        window.alert("invalid");
+    }
+}
